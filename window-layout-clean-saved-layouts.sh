89e4c40ba2f3b4261d8bc6d856d5518ff73fb7c4
Blaine Gardner <blaine.gardner@suse.com>
2018-02-05 09:40:36 -0700

    Add support for multiple users
    This hasn't been tested, but it should allow multiple users on the same
    machine to use the tool. It appends the username to the save location
    in /tmp and deletes all save locations at boot.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- window-layout-clean-saved-layouts.sh
+++ window-layout-clean-saved-layouts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm -rf /tmp/.window-layout/
+rm -rf /tmp/.window-layout-*/
 
 time="$(date)"
 echo "$time - Removed saved window layouts at boot" > "/tmp/window-layout-clean-saved-layouts.log"
